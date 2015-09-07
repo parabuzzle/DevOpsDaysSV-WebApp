e56f83e43bf51e329af5b051cf28e6e9bf581366
Mike Heijmans <parabuzzle@gmail.com>
2015-09-07 05:50:01 -0700

    less root more security

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 echo "stopping running application"
-ssh root@104.236.161.220 'docker stop dodsv'
-ssh root@104.236.161.220 'docker rm dodsv'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker stop dodsv'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker rm dodsv'
 
 echo "pulling latest version of the code"
-ssh root@104.236.161.220 'docker pull parabuzzle/devopsdayssv-webapp:latest'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker pull parabuzzle/devopsdayssv-webapp:latest'
 
 echo "starting the new version"
-ssh root@104.236.161.220 'docker run -d --restart=always --name dodsv -p 80:4567 parabuzzle/devopsdayssv-webapp:latest'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker run -d --restart=always --name dodsv -p 80:4567 parabuzzle/devopsdayssv-webapp:latest'
 
 echo "success!"
 
